f65a4acac6be71f8c845bcfb5724eca2a95b2bff
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2019-03-20 15:51:21 +0000

    some examples of annotations and javassist were added

---
+++ src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler.java
@@ -0,0 +1,48 @@
+package ist.meic.pa.FunctionalProfiler;
+
+import javassist.*;
+import java.util.*;
+
+class WithFunctionalProfiler {
+
+    public static void main(String[] args) {
+        printResult();
+    }
+
+    private static void printResult() {
+        System.out.println(String.format("Total reads: %s Total writes: %s", 6, 1));
+        System.out.println(String.format("class FunctionalCounter -> reads: %s writes: %s", 3, 0));
+        System.out.println(String.format("class ImperativeCounter -> reads: %s writes: %s", 3, 1));
+    }
+
+    private static void loadByteCode() {
+        /*ClassPool cp = ClassPool.getDefault();
+        ClassFile cf = cp.get("com.baeldung.javasisst.Point").getClassFile();
+        MethodInfo minfo = cf.getMethod("move");
+        CodeAttribute ca = minfo.getCodeAttribute();
+        CodeIterator ci = ca.iterator();
+ 
+        List<String> operations = new LinkedList<>();
+        while (ci.hasNext()) {
+            int index = ci.next();
+            int op = ci.byteAt(index);
+            operations.add(Mnemonic.OPCODE[op]);
+        }*/
+    }
+
+    private static void iterateAnnotations() {
+        /*int passed = 0, failed = 0;
+        for (Method m : Class.forName(args[0]).getMethods()) {
+            if (m.isAnnotationPresent(Test.class)) {
+                try {
+                    m.invoke(null);
+                    passed++;
+                } catch (Throwable ex) {
+                    System.out.printf("Test %s failed: %s %n", m, ex.getCause());
+                    failed++;
+                }
+            }
+        }
+        System.out.printf("Passed: %d, Failed %d%n", passed, failed);*/
+    }
+}
