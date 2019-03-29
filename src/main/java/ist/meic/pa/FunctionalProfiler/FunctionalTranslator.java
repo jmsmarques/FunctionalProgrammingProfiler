f40af8f49a8a1eff2d78db70429dd8c92402c125
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2019-03-29 18:26:24 +0000

    deleted comments

--- src/main/java/ist/meic/pa/FunctionalProfiler/FunctionalTranslator.java
+++ src/main/java/ist/meic/pa/FunctionalProfiler/FunctionalTranslator.java
@@ -7,10 +7,13 @@ import javassist.expr.FieldAccess;
 import javassist.bytecode.MethodInfo;
 import java.util.*;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.Class;
 
 public class FunctionalTranslator implements Translator {
     public static Set<String> addedFields = new HashSet<String>();
     public static Set<String> countIncr = new HashSet<String>();
+    public static Set<CtClass> teste = new HashSet<CtClass>();
 
     @Override
     public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
@@ -26,48 +29,25 @@ public class FunctionalTranslator implements Translator {
         //System.out.println("Find Read Write");
 
         findWriteRead(pool,ctClass,addedFields,countIncr);
+
+        
         // for(String added: this.addedFields){
         //     System.out.println("ADDED FIELDS: " + added);
         // }
     }
 
-
-
-    private void findMethods(ClassPool pool,CtClass cc){
-        for(CtMethod method: cc.getDeclaredMethods()){
-            try {
-                System.out.println("Method: " + method.getName());
-                method.instrument(new ExprEditor() {
-                    public void edit(MethodCall mc) throws CannotCompileException {
-                        try {
-                            CtClass ctclass = pool.get(mc.getClassName());
-                            CtMethod ctmethod = mc.getMethod();
-                            System.out.println("ClassName: " + ctclass.getName() + " | MethodName: " + ctmethod.getName());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-
     public void addCounters(CtClass ctclass,FieldAccess fa,CtMethod cmethod,Set<String> countInc) throws CannotCompileException,NotFoundException,IOException{
-        System.out.println("METHOD: " + cmethod.getName());
+        //System.out.println("METHOD: " + cmethod.getName());
 
         if (fa.isReader()){
-            if(countInc.add(fa.getClassName() + cmethod.getName() + "read")){
-                cmethod.insertBefore("{System.out.println(\"Increment!!!\");countRead++;}");
+            if(countInc.add(fa.getClassName() + " " + cmethod.getName() + " read")){
+                cmethod.insertBefore("{countRead++;System.out.println(\"CountRead:\" + countRead);}");
                 //ctclass.toClass();
             }
         }
         else if(fa.isWriter()){
-            if(countInc.add(fa.getClassName() + cmethod.getName() + "write")){
-                cmethod.insertBefore("{System.out.println(\"Increment!!!\"); countWrite++;}");
+            if(countInc.add(fa.getClassName() + " " + cmethod.getName() + " write")){
+                cmethod.insertBefore("{countWrite++;System.out.println(\"CountWrite:\" + countWrite);}");
                 //ctclass.toClass();
             }
         }
@@ -84,7 +64,8 @@ public class FunctionalTranslator implements Translator {
                                     if (!fa.getClassName().toLowerCase().contains("java".toLowerCase())){
                                         CtClass ctclass = pool.get(fa.getClassName());
                                         ctclass.defrost();
-                                        System.out.println("Class: " + ctclass.getName());
+                                        //System.out.println("Class: " + ctclass.getName());
+                                        if(teste.add(cc));
                                         if(addedFields.add(fa.getClassName())){
                                             CtField countReadField = CtField.make("public static int countRead = 0;",ctclass);
                                             CtField countWriteField = CtField.make("public static int countWrite = 0;",ctclass);
--- src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler.java
+++ src/main/java/ist/meic/pa/FunctionalProfiler/WithFunctionalProfiler.java
@@ -30,25 +30,23 @@ class WithFunctionalProfiler {
                 //append the path automatically not working...
                 //pool.appendClassPath(new LoaderClassPath(Thread.currentThread().getContextClassLoader()));
                 Loader classLoader = new Loader(pool);
-                classLoader.delegateLoadingOf​("ist.meic.pa");
                 classLoader.addTranslator(pool, translator);
-                classLoader.run("ist.meic.pa.FunctionalProfiler." + args[0], null);
+                //classLoader.delegateLoadingOf("ist.meic.pa.FunctionalProfiler.");
+                args[0] = "ist.meic.pa.FunctionalProfiler." + args[0];
+                classLoader.run(args);
+            
                 for(String added: FunctionalTranslator.addedFields){
-                    System.out.println("ADDED FIELDS: " + added);
+                    //System.out.println("ADDED FIELDS: " + added);
                     printCounter(pool,added);
                 }
-                for(String incr: FunctionalTranslator.countIncr){
+                /*for(String incr: FunctionalTranslator.countIncr){
                     System.out.println("INCRE FIELDS: " + incr);
-                }
+                }*/
                 //loadByteCode("ist.meic.pa.FunctionalProfiler." + args[0]);
             } catch (Throwable e) {
                 e.printStackTrace();
             }
         }
-
-
-        //loadByteCode("ist.meic.pa.FunctionalProfiler.FunctionalCounter");
-        //loadByteCode("ist.meic.pa.FunctionalProfiler.ImperativeCounter");
     }
 
 
@@ -56,24 +54,43 @@ class WithFunctionalProfiler {
     private static void printCounter(ClassPool pool,String classname) throws ClassNotFoundException, NoSuchFieldException{
         Class cls = Class.forName(classname);
         Field[] f = cls.getDeclaredFields();
-        for(Field field: f){
+        /*for(Field field: f){
             System.out.println("FIELD NAME: " + field.getName());
-        }
-        System.out.println("Print class: " + cls.getName());
+        }*/
+        //System.out.println("Print class: " + cls.getName());
         Field read = cls.getDeclaredField("countRead");
         Field write = cls.getDeclaredField("countWrite");
         read.setAccessible(true);
         write.setAccessible(true);
         try {
-            int nread = read.getInt(cls);
-            int nwrite = write.getInt(cls);
+            int nread = read.getInt(null);
+            int nwrite = write.getInt(null);
             System.out.println("class " + cleanClassName(classname) + " -> " + "reads: " + nread + " writes: " + nwrite);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+    }
 
+    private static void ctprintCounter(ClassPool pool, CtClass cls) throws ClassNotFoundException, NoSuchFieldException, NotFoundException{
+        CtField[] f = cls.getDeclaredFields();
+        /*for(Field field: f){
+            System.out.println("FIELD NAME: " + field.getName());
+        }*/
+        //System.out.println("Print class: " + cls.getName());
+        CtField read = cls.getDeclaredField("countRead");
+        CtField write = cls.getDeclaredField("countWrite");
+        System.out.println("hue: " + read);
+        //read.setAccessible(true);
+        //write.setAccessible(true);
+        /*try {
+            int nread = read.getInt(cls);
+            int nwrite = write.getInt(cls);
+            //System.out.println("class " + cleanClassName(classname) + " -> " + "reads: " + nread + " writes: " + nwrite);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 
     private static String cleanClassName(String classname){
@@ -87,113 +104,4 @@ class WithFunctionalProfiler {
         System.out.println(String.format("class FunctionalCounter -> reads: %s writes: %s", 3, 0));
         System.out.println(String.format("class ImperativeCounter -> reads: %s writes: %s", 3, 1));
     }
-
-    private static void loadByteCode(String className) {
-        ClassPool cp = ClassPool.getDefault();
-        ArrayList<String> methodsNames = new ArrayList<String>();
-        try {
-            System.out.println("ClassName: " + className);
-            CtClass cc = cp.get(className);
-            System.out.println(cc.getName());
-            CtMethod[] methods = cc.getDeclaredMethods();
-            for(int i = 0;i< methods.length;i++){
-                System.out.println("Methods: " +methods[i].getName());
-                methodsNames.add(methods[i].getName());
-            }
-            CtField[] fields = cc.getDeclaredFields();
-
-            for(int i = 0;i< fields.length;i++){
-                System.out.println("Fields: " + fields[i].getName());
-            }
-            parametersByMethod(cc,methodsNames);
-            //System.out.println(cc.getClassFile());
-        }
-        catch (NotFoundException e) {
-            System.out.println("Class doesnt exist...");
-        }
-        
-
-        /*ClassFile cf = cp.get("com.baeldung.javasisst.Point").getClassFile();
-        MethodInfo minfo = cf.getMethod("move");
-        CodeAttribute ca = minfo.getCodeAttribute();
-        CodeIterator ci = ca.iterator();
- 
-        List<String> operations = new LinkedList<>();
-        while (ci.hasNext()) {
-            int index = ci.next();
-            int op = ci.byteAt(index);
-            operations.add(Mnemonic.OPCODE[op]);
-        }*/
-    }
-
-
-    private static void parametersByMethod(CtClass cc,ArrayList<String> methodsNames){
-        for(int i = 0;i< methodsNames.size();i++){
-            try{
-                cc.defrost();
-                CtMethod cmethod = cc.getDeclaredMethod(methodsNames.get(i));
-                cmethod.instrument(new ExprEditor() {
-                    public void edit(MethodCall m) throws CannotCompileException {
-                        System.out.println("Method: " + m.getMethodName() + "--line: " + m.getLineNumber());
-                    }
-                });
-            }catch(NotFoundException | CannotCompileException e){
-                System.out.println("Method not found...");
-            }
-        }
-    }
-
-    private static void iterateAnnotations() {
-        /*int passed = 0, failed = 0;
-        for (Method m : Class.forName(args[0]).getMethods()) {
-            if (m.isAnnotationPresent(Test.class)) {
-                try {
-                    m.invoke(null);
-                    passed++;
-                } catch (Throwable ex) {
-                    System.out.printf("Test %s failed: %s %n", m, ex.getCause());
-                    failed++;
-                }
-            }
-        }
-        System.out.printf("Passed: %d, Failed %d%n", passed, failed);*/
-    }
 }
-
-
-// private static void parametersByMethod(CtClass cc,ArrayList<String> methodsNames){
-//     try{
-//         for(int i = 0;i< methodsNames.size();i++){
-//             System.out.println("Inside ParametersbyMethod");
-//             CtMethod method = cc.getDeclaredMethod(methodsNames.get(i));
-//             System.out.println("Method name: " + method.getName());
-//             if (method.getMethodInfo() != null){
-//                 MethodInfo methodInfo = method.getMethodInfo();
-//                 System.out.println("Before");
-//                 CodeAttribute ca = methodInfo.getCodeAttribute();
-//                 CodeIterator ci = ca.iterator();
-//                 System.out.println("After iterator");
-//                 while(ci.hasNext()){
-//                     int index = ci.next();
-//                     int op = ci.byteAt(index);
-//                     System.out.println("OpCOde: " + op);
-//                     System.out.println("Iterator: " + Mnemonic.OPCODE[op]);
-//                     if (op == 180) {
-//                         int a1 = ci.s16bitAt(index + 1);
-//                         String fieldName = " " + cc.getClassFile().getConstPool().getFieldrefName(a1); 
-//                         System.out.println("field name: " + fieldName);
-//                     }
-//                 }
-//                 LocalVariableAttribute table = (LocalVariableAttribute) methodInfo.getCodeAttribute().getAttribute(javassist.bytecode.LocalVariableAttribute.tag);
-//                 System.out.println("Parametrs: " + method.getMethodInfo());
-//                 System.out.println("Table length: " + table.tableLength());
-//                 for(int j = 0; j < table.tableLength();j++){
-//                     System.out.println(table.variableName(j));
-//                 }
-//             }
-//         }
-//     }
-//     catch(Exception e){
-//         System.out.println("Dont have methods...");
-//     }
-// }
